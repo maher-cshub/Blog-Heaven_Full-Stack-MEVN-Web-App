a1fbe2e91bbf1e6bb2d5c52f73a227ecd1814173
maher-cshub <maherarab.1299@gmail.com>
2023-05-18 15:20:06 +0100

    fix uploads

--- server/routes/postRouter.js
+++ server/routes/postRouter.js
@@ -2,13 +2,14 @@ const postRouter = require('express').Router();
 
 const postController = require('../controllers/postController');
 
+import { API_URI } from '../../client/src/secret';
 //multer for file upload
 const multer = require('multer');
 
 //multer middleware
 let storage = multer.diskStorage({
     destination: function(req,file,cb) {
-        cb(null,'/uploads');
+        cb(null,`${API_URI}/uploads`);
     },
     filename: function(req,file,cb) {
         cb(null,file.fieldname + "_" + Date.now() + "_" + file.originalname);
